50c86d2dc7502892d94564014369fa7021582b24
Ashwin <ashwinsriramulu@hotmail.com>
2026-02-27 21:42:19 +0530

    Implemented the Wait and Manual Approval logic

--- NewOrderOrchestration.cs
+++ NewOrderOrchestration.cs
@@ -1,4 +1,6 @@
 using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.DurableTask;
@@ -7,18 +9,27 @@ using Microsoft.Extensions.Logging;
 
 namespace MyOrg.AzureFuncs;
 
+
 public class OrderOrchestrationInput
 {
     public Guid OrderId { get; set; }
     public string[] SeatNumbers {get; set;} = [];
+    public decimal TotalPrice { get; set; }
+}
+
+public class ApprovalResult
+{
+    public bool IsApproved { get; set; }
+    public string ApprovedBy { get; set; } = string.Empty;
 }
 
 public static class NewOrderOrchestration
 {
+    public const string OrderApprovalEventName = "OrderApproval";
+
     [Function(nameof(NewOrderOrchestration))]
     public static async Task<List<string>> RunOrchestrator(
-        [OrchestrationTrigger] TaskOrchestrationContext context)
-    {
+        [OrchestrationTrigger] TaskOrchestrationContext context) {
         ILogger logger = context.CreateReplaySafeLogger(nameof(NewOrderOrchestration));
         logger.LogInformation("Saying hello.");
 
@@ -35,6 +46,31 @@ public static class NewOrderOrchestration
         // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
         */
 
+        // check if the price is more than $500, need approval
+        if (orderInput.TotalPrice > 500)
+        {
+           logger.LogInformation("Order {orderId} requires manual approval.", orderInput.OrderId);
+           await context.CallActivityAsync(nameof(RequestApproval), orderInput.OrderId);
+
+            try
+            {
+                var result = await context.WaitForExternalEvent<ApprovalResult>(
+                    OrderApprovalEventName, TimeSpan.FromMinutes(1));
+
+                if (!result.IsApproved)
+                {
+                    logger.LogInformation("Order {orderId} is rejected by {approver}.", orderInput.OrderId, result.ApprovedBy);
+                    return [$"Order {orderInput.OrderId} rejected by {result.ApprovedBy}."];
+                }
+            }
+
+            catch (TaskCanceledException)
+            {
+                logger.LogInformation("waiting for approval for order {orderId} timed out.", orderInput.OrderId);
+                return [$"waiting for approval for order {orderInput.OrderId} timed out."];
+            }
+        }
+        // run activities in parallel
         var tasks = new List<Task<string>>();
 
         foreach (var seat in orderInput.SeatNumbers)
@@ -44,6 +80,14 @@ public static class NewOrderOrchestration
 
         string[] results = await Task.WhenAll(tasks);
         return [.. results];
+    }
+
+    [Function(nameof(RequestApproval))]
+    public static void RequestApproval([ActivityTrigger] Guid orderId,     
+    FunctionContext executionContext)
+    {
+        ILogger logger = executionContext.GetLogger("RequestApproval");
+        logger.LogInformation("Requesting approval for order {orderId}.", orderId);
     }
 
     [Function(nameof(CreateTicket))]
@@ -58,22 +102,49 @@ public static class NewOrderOrchestration
 
     [Function("NewOrderOrchestration_HttpStart")]
     public static async Task<HttpResponseData> HttpStart(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
         [DurableClient] DurableTaskClient client,
         FunctionContext executionContext)
     {
         ILogger logger = executionContext.GetLogger("NewOrderOrchestration_HttpStart");
 
+        var price = req.Query["price"] ?? "100";
+
         var inputData = new OrderOrchestrationInput
         {
             OrderId = Guid.NewGuid(),
-            SeatNumbers = new string[] { "A1", "A2", "A3" }
+            SeatNumbers = ["A1", "A2", "A3"],
+            TotalPrice = decimal.Parse(price)
         };
 
         string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
             nameof(NewOrderOrchestration), inputData);
         logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
 
+        // Returns an HTTP 202 response with an instance management payload.
+        // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
+
         return await client.CreateCheckStatusResponseAsync(req, instanceId);
     }
+
+    [Function("NewOrderOrchestration_Approve")]
+    public static async Task<IActionResult> Approve(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+        [DurableClient] DurableTaskClient client,
+        FunctionContext executionContext)
+    {
+        ILogger logger = executionContext.GetLogger("NewOrderOrchestration_Approve");
+        var approvalResult = await req.ReadFromJsonAsync<ApprovalResult>();
+        
+        var id = req.Query["id"];
+        if  (string.IsNullOrEmpty(id) || approvalResult == null)
+        {
+            return new BadRequestObjectResult("Please provide instance ID and approval result in the request.");
+        }
+
+        logger.LogInformation("Raising approval event for instance ID = '{instanceId}'.", id);
+        await client.RaiseEventAsync(id, OrderApprovalEventName, approvalResult);
+        return new OkObjectResult($"Approval result for instance ID = '{id}' has been sent.");
+    }
+    
 }
